ff151aa1d01236a49e7245582cea8f03fe9c377d
abdulqudus001 <abdulqudusabubakre@gmail.com>
2020-09-29 23:35:39 +0100

    Added search functionality

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
+import VueScrollTo from 'vue-scrollto'
+import VueFuse from 'vue-fuse'
 import DefaultLayout from '~/layouts/Default.vue'
 require('typeface-nunito');
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+
+  Vue.use(VueScrollTo, {
+    duration: 500,
+    easing: "ease",
+  })
+
+  Vue.use(VueFuse)
 }
